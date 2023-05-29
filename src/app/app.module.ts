a314245c1bebfe1eedebf3337b6910000475d09e
Fahmi Pamungkas <fame1302@gmail.com>
2023-05-29 17:03:19 +0700

    update sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import {MatIconModule} from "@angular/material/icon";
     BrowserAnimationsModule,
     MatSidenavModule,
     MatButtonToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
