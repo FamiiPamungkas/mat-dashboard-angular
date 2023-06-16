17470a83f837ab78dd96196aac90eaf3acaff14b
Fahmi Pamungkas <fame1302@gmail.com>
2023-06-16 10:29:38 +0700

    add header, and list menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,23 @@ import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {MatIconModule} from "@angular/material/icon";
 import {MatListModule} from "@angular/material/list";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatListModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSidenavModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatListModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -7,5 +7,7 @@ import {Component} from '@angular/core';
 })
 export class DashboardComponent {
 
-
+  log() {
+    console.log("testt")
+  }
 }
