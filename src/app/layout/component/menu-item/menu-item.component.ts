04e9762ecf4e51fb98582fa6e01a67c1bcb59c3e
Fahmi Pamungkas <fame1302@gmail.com>
2023-07-03 14:53:40 +0700

    update: Menu Item & Sub Menu navigation location href -> router

--- src/app/layout/component/menu-item/menu-item.component.ts
+++ src/app/layout/component/menu-item/menu-item.component.ts
@@ -1,6 +1,7 @@
 import {AfterContentInit, Component, ContentChildren, Input, QueryList} from '@angular/core';
 import {SubMenuComponent} from "../sub-menu/sub-menu.component";
 import {rotateArrowAnimation, submenuToggleAnimation} from "../../../utility/constant";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'menu-item',
@@ -12,23 +13,28 @@ export class MenuItemComponent implements AfterContentInit {
   @Input() icon: string = "";
   @Input() label: string = "";
   @Input() active: boolean = false;
-  @Input() link :string = "";
+  @Input() link: string = "";
   @ContentChildren(SubMenuComponent) submenus?: QueryList<SubMenuComponent>;
 
   hasSubmenu: boolean = false;
   submenuState: string = 'closed';
+
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngAfterContentInit() {
     if (this.submenus && this.submenus?.length > 0) {
       this.hasSubmenu = true;
     }
 
-    this.submenuState = this.active ? 'open':'closed';
+    this.submenuState = this.active ? 'open' : 'closed';
   }
 
   toggleSubmenu() {
-    if (!this.hasSubmenu){
-      location.href = this.link;
+    if (!this.hasSubmenu) {
+      this.router.navigateByUrl(this.link).then(r => false);
       return;
     }
 
--- src/app/layout/component/sub-menu/sub-menu.component.ts
+++ src/app/layout/component/sub-menu/sub-menu.component.ts
@@ -1,21 +1,27 @@
 import {AfterContentInit, Component, ContentChildren, Input, QueryList} from '@angular/core';
 import {rotateArrowAnimation, submenuToggleAnimation} from "../../../utility/constant";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'sub-menu',
   templateUrl: './sub-menu.component.html',
   styleUrls: ['./sub-menu.component.css'],
-  animations:[submenuToggleAnimation(), rotateArrowAnimation()]
+  animations: [submenuToggleAnimation(), rotateArrowAnimation()]
 })
 export class SubMenuComponent implements AfterContentInit {
   @Input() label: string = "";
   @Input() active: boolean = false;
-  @Input() link:string = "";
+  @Input() link: string = "";
   @Input() level: number = 1;
   @ContentChildren(SubMenuComponent) submenus?: QueryList<SubMenuComponent>;
 
   hasSubmenu: boolean = false;
   submenuState: string = 'closed';
+
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngAfterContentInit() {
     if (this.submenus && this.submenus?.length > 0) {
@@ -23,9 +29,9 @@ export class SubMenuComponent implements AfterContentInit {
     }
   }
 
-  toggleSubmenu(){
-    if (!this.hasSubmenu){
-      location.href = this.link;
+  toggleSubmenu() {
+    if (!this.hasSubmenu) {
+      this.router.navigateByUrl(this.link).then(r => false);
       return;
     }
 
