ac395447392e15b1ec082bcf736f425ce6140198
Fahmi Pamungkas <fame1302@gmail.com>
2023-08-15 14:07:54 +0700

    update : access.guard.ts more strict, path must be exist in user->role->menus

--- src/app/guard/access.guard.ts
+++ src/app/guard/access.guard.ts
@@ -2,7 +2,7 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from "../service/auth.service";
 
-export const accessGuard: CanActivateFn = (route, state) => {
+export const accessGuard: CanActivateFn = (route) => {
   const authService = inject(AuthService);
   const router = inject(Router);
   const user = authService.authUser;
@@ -12,8 +12,19 @@ export const accessGuard: CanActivateFn = (route, state) => {
 
   } else {
     let haveAccess: boolean = false;
+
+    // remove path variable / param
+    let cleanPath = [...route.url];
+    for (let paramsKey in route.params) {
+      const index = cleanPath.findIndex((s) => s.path == route.params[paramsKey]);
+      if (index > 0) {
+        cleanPath.splice(index, 1);
+      }
+    }
+    const realPath: string = `/${cleanPath.join("/")}`;
+
     for (let role of user.roles) {
-      const exists = role.menus.find(menu => state.url.includes(menu.link)) != undefined;
+      const exists = role.menus.find(menu => realPath === menu.link) != undefined;
       if (exists) haveAccess = true;
     }
 
